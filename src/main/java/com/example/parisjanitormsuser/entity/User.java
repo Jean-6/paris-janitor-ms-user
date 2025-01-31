281041b5453cd8447a75a30de286332166353587
Jean <oka.jeanjaures@gmail.com>
2025-01-31 01:19:30 +0100

    feat: implement user model to authentication

---
+++ src/main/java/com/example/parisjanitormsuser/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.parisjanitormsuser.entity;
+
+import com.example.parisjanitormsuser.security.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+}
---
+++ src/main/java/com/example/parisjanitormsuser/security/enums/Privilege.java
@@ -0,0 +1,8 @@
+package com.example.parisjanitormsuser.security.enums;
+
+public enum Privilege {
+    READ_PRIVILEGE,
+    WRITE_PRIVILEGE,
+    UPDATE_PRIVILEGE,
+    DELETE_PRIVILEGE
+}
