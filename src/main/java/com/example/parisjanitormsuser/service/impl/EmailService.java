447f00875cfa33a9a883cc21b84b163eda1d71b5
Jean <oka.jeanjaures@gmail.com>
2025-02-26 02:41:01 +0100

    feat (service): implement email sender service

--- src/main/java/com/example/parisjanitormsuser/service/impl/EmailService.java
+++ src/main/java/com/example/parisjanitormsuser/service/impl/EmailService.java
@@ -3,6 +3,7 @@ package com.example.parisjanitormsuser.service.impl;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,20 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    @Autowired
+    private JavaMailSender mailSender;
 
     public void sendResetEmail(String to,String token) throws MessagingException {
-        String resetLink = "";
+        String resetLink = "http://localhost:9090/api/auth/reset-password?token="+token;
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
         helper.setTo(to);
-        helper.setSubject("Reset Password");
-        helper.setText(resetLink, true);
+        helper.setSubject("Réinitialisation de votre mot de passe");
+        helper.setText("Cliquer sur ce lien pour réinitialiser votre mot de passe : " + resetLink, true);
 
         mailSender.send(mimeMessage);
     }
