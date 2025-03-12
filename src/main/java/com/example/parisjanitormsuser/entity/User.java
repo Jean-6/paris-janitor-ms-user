1f960a0093e12c50f3d3d7198e1360616d9bb49d
Jean <oka.jeanjaures@gmail.com>
2025-03-12 02:54:26 +0100

    perf : implements entity and dto for authentication

--- src/main/java/com/example/parisjanitormsuser/entity/User.java
+++ src/main/java/com/example/parisjanitormsuser/entity/User.java
@@ -18,7 +18,7 @@ import java.util.Collection;
 @AllArgsConstructor
 @Data
 @Entity
-@Table(name = "_user")
+@Table(name = "user")
 public class User implements UserDetails {
 
     @jakarta.persistence.Id
