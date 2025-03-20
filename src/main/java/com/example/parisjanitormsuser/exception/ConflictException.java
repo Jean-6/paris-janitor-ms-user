68faea9f842a6449787c2b32e60d989c9da55de5
Jean <oka.jeanjaures@gmail.com>
2025-03-20 23:31:56 +0100

    config: implement exceptions about request entities

---
+++ src/main/java/com/example/parisjanitormsuser/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.example.parisjanitormsuser.exception;
+
+public class ConflictException  extends RuntimeException{
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/parisjanitormsuser/exception/GlobalException.java
@@ -0,0 +1,51 @@
+package com.example.parisjanitormsuser.exception;
+
+
+
+import com.example.parisjanitormsuser.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.lang.IllegalArgumentException;
+
+
+@ControllerAdvice
+public class GlobalException {
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND.value())
+                .body(new ErrorResponse("","NOT_FOUND",""));//"Resource Not Found"
+    }
+
+    @ExceptionHandler(java.lang.IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleBadRequest(IllegalArgumentException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(new ErrorResponse("","BAD_REQUEST",""));//"Bad Request"
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorResponse> handleConflictException(ConflictException ex){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT.value())
+                .body(new ErrorResponse("","CONFLICT",""));//"Conflict"
+    }
+
+    @ExceptionHandler(GenericException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .body(new ErrorResponse("","INTERNAL_SERVER_ERROR",""));//"Internal Server Error"
+    }
+
+}
