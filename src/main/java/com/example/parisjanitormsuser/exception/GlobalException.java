07a838902580fd8018adcb2f7a5365e8d2a20658
Jean <oka.jeanjaures@gmail.com>
2025-07-07 16:51:10 +0200

    fix : centralize exception handler

--- src/main/java/com/example/parisjanitormsuser/exception/GlobalException.java
+++ src/main/java/com/example/parisjanitormsuser/exception/GlobalException.java
@@ -1,45 +1,42 @@
 package com.example.parisjanitormsuser.exception;
 
-
-
-import com.example.parisjanitormsuser.common.ErrorMsg;
-import com.example.parisjanitormsuser.dto.ErrorRes;
 import com.example.parisjanitormsuser.dto.ResponseWrapper;
-import com.example.parisjanitormsuser.security.exception.BadCredentialsException;
 import com.example.parisjanitormsuser.security.exception.InvalidDataException;
 import com.example.parisjanitormsuser.security.exception.UnauthorizedException;
 import com.example.parisjanitormsuser.security.exception.UserAlreadyExistsException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalException {
 
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ResponseWrapper<Object>> userAlreadyExistsException(UserAlreadyExistsException ex){
+    public ResponseEntity<ResponseWrapper<Object>> userAlreadyExistsException(UserAlreadyExistsException ex, HttpServletRequest request){
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ResponseWrapper.conflict(ErrorMsg.USER_ALREADY_EXISTS));
+                .body(ResponseWrapper.error(null,ex.getMessage(),HttpStatus.CONFLICT.value(), request.getRequestURI()));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ResponseWrapper<Object>> badCredentials(BadCredentialsException ex) {
+    public ResponseEntity<ResponseWrapper<Object>> badCredentials(BadCredentialsException ex, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ResponseWrapper.badRequest(ErrorMsg.BAD_CREDENTIALS));
+                .body(ResponseWrapper.error(null,ex.getMessage(),HttpStatus.BAD_REQUEST.value(), request.getRequestURI()));
     }
 
     @ExceptionHandler(InvalidDataException.class)
-    public ResponseEntity<ResponseWrapper<Object>> invalidData(InvalidDataException ex) {
+    public ResponseEntity<ResponseWrapper<Object>> invalidData(InvalidDataException ex, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ResponseWrapper.badRequest(ErrorMsg.BAD_CREDENTIALS));
+                .body(ResponseWrapper.error(null,ex.getMessage(),HttpStatus.BAD_REQUEST.value(), request.getRequestURI()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<?> unauthorized(UnauthorizedException ex) {
+    public ResponseEntity<?> unauthorized(UnauthorizedException ex, HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(ResponseWrapper.unauthorized(ErrorMsg.UNAUTHORIZED));
+                .body(ResponseWrapper.error(null,ex.getMessage(),HttpStatus.UNAUTHORIZED.value(), request.getRequestURI()));
     }
 }
