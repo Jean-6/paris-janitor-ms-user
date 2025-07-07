e16d0fc604bdf20b8f665754d2d20d27da14fe7b
Jean <oka.jeanjaures@gmail.com>
2025-07-07 17:19:43 +0200

    refactor : fixing of access denied handler response

--- src/main/java/com/example/parisjanitormsuser/security/config/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/parisjanitormsuser/security/config/CustomAccessDeniedHandler.java
@@ -1,6 +1,6 @@
 package com.example.parisjanitormsuser.security.config;
 
-import com.example.parisjanitormsuser.security.handlers.ErrorResponse;
+import com.example.parisjanitormsuser.dto.ResponseWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -8,13 +8,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.Instant;
 
 // interface that handle refused access error 403, when authenticated user attempt to get protect resource
 // without having necessary resources
@@ -23,17 +23,18 @@ import java.time.Instant;
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
         log.error("Access denied error: {}", accessDeniedException.getMessage());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        com.example.parisjanitormsuser.security.handlers.ErrorResponse body = ErrorResponse.builder()
-                .status(HttpServletResponse.SC_FORBIDDEN)
-                .error("Forbidden")
-                .timestamp(Instant.now())
-                .message(accessDeniedException.getMessage())
-                .path(request.getServletPath())
-                .build();
+        ResponseWrapper<String> body = ResponseWrapper.error(
+                null,
+                accessDeniedException.getMessage(),/*Access denied*/
+                HttpStatus.FORBIDDEN.value(),
+                request.getRequestURI()
+        );
+
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
