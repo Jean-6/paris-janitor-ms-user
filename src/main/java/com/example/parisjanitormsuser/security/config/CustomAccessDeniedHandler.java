8594aebe023f0b6515362b4d366795a2154c9a97
Jean <oka.jeanjaures@gmail.com>
2025-07-08 01:26:11 +0200

    refactor: set response of access denied  handler

--- src/main/java/com/example/parisjanitormsuser/security/config/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/parisjanitormsuser/security/config/CustomAccessDeniedHandler.java
@@ -1,6 +1,6 @@
 package com.example.parisjanitormsuser.security.config;
 
-import com.example.parisjanitormsuser.dto.ResponseWrapper;
+import com.example.parisjanitormsuser.dto.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -8,13 +8,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Instant;
 
 // interface that handle refused access error 403, when authenticated user attempt to get protect resource
 // without having necessary resources
@@ -28,12 +29,13 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        ResponseWrapper<String> body = ResponseWrapper.error(
-                null,
-                accessDeniedException.getMessage(),/*Access denied*/
-                HttpStatus.FORBIDDEN.value(),
-                request.getRequestURI()
-        );
+        ErrorResponse body = ErrorResponse.builder()
+                .status(HttpServletResponse.SC_FORBIDDEN)
+                .error("Access Denied")
+                .timestamp(Instant.now())
+                .message(accessDeniedException.getMessage())
+                .path(request.getRequestURI())
+                .build();
 
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
