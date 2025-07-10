09943b920c32aad48344b63f79b1abc44b876a05
Jean <oka.jeanjaures@gmail.com>
2025-07-10 16:54:43 +0200

    refactor : enhance filter to intercept request within the scope of logging, CORS handler , JWT, etc...

--- src/main/java/com/example/parisjanitormsuser/config/GlobalErrorFilter.java
+++ src/main/java/com/example/parisjanitormsuser/config/GlobalErrorFilter.java
@@ -1,13 +1,22 @@
 package com.example.parisjanitormsuser.config;
 
-import com.example.parisjanitormsuser.dto.ResponseWrapper;
+import com.example.parisjanitormsuser.dto.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Instant;
 
+
+/**
+ * Global filter to catch  unhandled errors
+ * and return standardized JSON response
+ */
+@Component
 public class GlobalErrorFilter implements Filter {
 
     private final ObjectMapper objectMapper ;
@@ -17,35 +26,37 @@ public class GlobalErrorFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+    public void init(FilterConfig filterConfig) {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
         try{
-            chain.doFilter(request, response);
+            chain.doFilter(request, response); // Intercept request
         }catch(Exception ex){
-            // Handle global error
+
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
             HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+            httpServletResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             httpServletResponse.setContentType("application/json");
-            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            // Response of error standardized
-            ResponseWrapper<Void> responseWrapper = new ResponseWrapper<>(
-                    false,
-                    "Internal Server Error",
-                    null,
-                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                    request.getServletContext().getContextPath());
-            String jsonResponse = objectMapper.writeValueAsString(responseWrapper);
+            httpServletResponse.setCharacterEncoding("UTF-8");
+
+            ErrorResponse errorResponse = ErrorResponse.builder()
+                    .timestamp(Instant.now())
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                    .message(ex.getMessage())
+                    .path(httpServletRequest.getRequestURI())
+                    .build();
+
+            String jsonResponse = objectMapper.writeValueAsString(errorResponse);
             httpServletResponse.getWriter().write(jsonResponse);
+            httpServletResponse.getWriter().flush();
 
         }
     }
 
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
-    }
 }
